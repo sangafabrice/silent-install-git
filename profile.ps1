461748681865680013d2f36a58b1d03762e52c5d
Fabrice Sanga <69244030+sangafabrice@users.noreply.github.com>
2021-10-20 08:10:06 +0200

    add posh-git and system attribute to .git folder
    hoist $prompt to avoid copying prompt from posh-git

--- profile.ps1
+++ profile.ps1
@@ -1,9 +1,11 @@
-Import-Module oh-my-posh
-
 $Prompt = (Get-Command Prompt).ScriptBlock
+
+Import-Module -Name posh-git
+Import-Module -Name oh-my-posh
+
 Set-PoshPrompt -Theme rudolfs-light-fork
 $GitPrompt = (Get-Command Prompt).ScriptBlock
 
 filter prompt {
-    (Test-Path ($PWD.Path + '\.git')) ? (& $GitPrompt):(& $Prompt)
+    (Test-Path ($PWD.Path + '\.git')) ? $(attrib /d .git +s +h; & $GitPrompt):(& $Prompt)
 }
